8ae163eaaefd6c93091f3d36b786c35ece5543dc
Victor Chemov <chomovva@gmail.com>
2018-06-23 15:33:05 +0300

    v 0.0.19

--- src/scripts/app/nav-menus.js
+++ src/scripts/app/nav-menus.js
@@ -1,8 +1,8 @@
 /* Навигационное меню */
 jQuery( document ).ready( function () {
 	
-		var $touch = jQuery( '#burger' ),
-			$menu 	= jQuery( '#nav-list' ),
+		var $touch = jQuery( '#header-nav-burger' ),
+			$menu 	= jQuery( '#header-nav-list' ),
 			$submunus = $menu.find( '.sub-menu' );
 
 		jQuery( $touch ).on( 'click', function( e ) {
@@ -18,7 +18,34 @@ jQuery( document ).ready( function () {
 		});
 
 		$submunus.each( function () {
-			jQuery( this ).closest( 'li' ).find( '> a' ).append( '<i class="icon icon-plus"></i>' );
+			jQuery( this ).closest( 'li' ).find( '> a' ).append( '<i class="icon icon-down-arrow"></i>' );
+		} );
+
+} );
+
+
+
+/* Навигационное меню */
+jQuery( document ).ready( function () {
+	
+		var $touch = jQuery( '#top-nav-burger' ),
+			$menu 	= jQuery( '#top-nav-list' ),
+			$submunus = $menu.find( '.sub-menu' );
+
+		jQuery( $touch ).on( 'click', function( e ) {
+			e.preventDefault();
+			$menu.slideToggle();
+		});
+		
+		jQuery( window ).resize( function() {
+			var w = jQuery( window ).width();
+			if( w > 980 && $menu.is( ':hidden' ) ) {
+				$menu.removeAttr( 'style' );
+			}
+		});
+
+		$submunus.each( function () {
+			jQuery( this ).closest( 'li' ).find( '> a' ).append( '<i class="icon icon-down-arrow"></i>' );
 		} );
 
 } );
